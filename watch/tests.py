1cfffe7671351817fe166a335e6c0f566ff3e883
Brian Otieno <bryomajor@gmail.com>
2019-11-30 20:54:24 +0300

    initiate test case for the profile model

--- watch/tests.py
+++ watch/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    '''
+    Test case for the Profile class and it's behaviours
+    '''
+    def setUp(self):
+        '''
+        Method that will run before any test case under this class
+        '''
+        self.new_user = User(username = "bilal", email = "bilal@gmail.com", password = "dontbelittleyourself",)
+        self.new_user.save()
+
+        self.new_profile = Profile(user = self.new_user, neighbourhood = "kasarani")
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+
+    def test_instance(self):
+        '''
+        Test to confirm that the object is being instantiated correctly
+        '''
+        self.assertTrue(isinstance(self.new_profile, Profile))
