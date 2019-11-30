eceff80a41a417e07334dca5bedccd56e59486c3
Brian Otieno <bryomajor@gmail.com>
2019-11-30 21:37:15 +0300

    use signals to create and save user profiles

--- watch/models.py
+++ watch/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Neighbourhood(models.Model):
@@ -9,3 +11,16 @@ class Neighbourhood(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    
+    @receiver(post_save, sender = User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender = User)
+
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def save_user_profile(self):
+        self.save()
