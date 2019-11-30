ca26b8be671c9fdf94783f11b2b9f536a0be4a49
Brian Otieno <bryomajor@gmail.com>
2019-11-30 21:06:53 +0300

    set up a neighbourhood model

--- watch/models.py
+++ watch/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    neighbourhood_name = models.CharField(max_length = 60)
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
