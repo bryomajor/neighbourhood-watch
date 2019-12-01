77e6c412806b1be2a0c092c24122295a974175e7
Brian Otieno <bryomajor@gmail.com>
2019-12-01 19:37:29 +0300

    create a view function for the blogposts

--- watch/views.py
+++ watch/views.py
@@ -31,6 +31,15 @@ def notification(request):
     return render(request, 'notifications.html', {"notifications":all_notifications})
 
 
+@login_required(login_url='/accounts/login/')
+def blog(request):
+    current_user=request.user
+    profile = Profile.objects.get(username=current_user)
+    blogposts = BlogPost.objects.filter(neighbourhood=profile.neighbourhood)
+
+    return render(request, 'blog.html', {"blogposts":blogposts})
+
+
 @login_required(login_url='/accounts/login')
 def create_profile(request):
     current_user=request.user
