cbe03740c2d44562780eed46ab9f04cc9d3c9e5d
Brian Otieno <bryomajor@gmail.com>
2019-12-01 14:34:32 +0300

    create a form where we can add a comment from

--- watch/forms.py
+++ watch/forms.py
@@ -21,3 +21,7 @@ class BusinessForm(forms.ModelForm):
         model = Business
         exclude = ['owner', 'neighbourhood']
 
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['username', 'post']
