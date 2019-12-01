294d8a7d2b4b64a493764c9331b964581dd54156
Brian Otieno <bryomajor@gmail.com>
2019-12-01 13:34:05 +0300

    set up health services model

--- watch/models.py
+++ watch/models.py
@@ -35,11 +35,21 @@ class Neighbourhood(models.Model):
 class notifications(models.Model):
     title = models.CharField(max_length=100)
     notification = HTMLField()
-    priority = models.CharField(max_length=15)
+    priority = models.CharField(max_length=15, choices=Priority, default="Informational")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE
     )
     post_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class healthservices(models.Model):
+    healthservices = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.healthservices
 
 
 class Profile(models.Model):
