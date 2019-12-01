84cf3056f66dc871b148fa91895ba0641ff810fa
Brian Otieno <bryomajor@gmail.com>
2019-12-01 13:49:48 +0300

    include a business model

--- watch/models.py
+++ watch/models.py
@@ -59,6 +59,20 @@ class healthservices(models.Model):
         cls.objects.filter(healthservices=healthservices).delete()
 
 
+class Business(models.Model):
+    logo = models.ImageField(upload_to='businesslogo/')
+    description = HTMLField()
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.CharField(max_length=100)
+    contact = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
 class Profile(models.Model):
     avatar = models.ImageField(upload_to='avatars/', blank = True)
     username = models.ForeignKey(User, on_delete=models.CASCADE)
