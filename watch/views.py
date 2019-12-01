5a13d313125f8359eac7370eaf0129a8a9c24d13
Brian Otieno <bryomajor@gmail.com>
2019-12-01 19:30:37 +0300

    create index, notification and the create-profile views

--- watch/views.py
+++ watch/views.py
@@ -1,6 +1,47 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Neighbourhood, healthservices, Business, Health, Authorities, BlogPost, Profile, notifications, Comment
+from .forms import notificationsForm, ProfileForm, BlogPostForm, BusinessForm, CommentForm
+import datetime as datetime
+import json
+from django.db.models import Q
+from django.contrib.auth.models import User
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('blah!')
+def index(request):
+    try:
+        if not request.user.is_authenticated:
+            return redirect('/accounts/login/')
+        current_user = request.user
+        profile = Profile.objects.get(username=current_user)
+    except ObjectDoesNotExist:
+        return redirect('create-profile')
+
+    return render(request, 'index.html')
+
+
+@login_required(login_url='/accounts/login/')
+def notification(request):
+    current_user = request.user
+    profile = Profile.objects.get(username=current_user)
+    all_notifications = notifications.objects.filter(neighbourhood=profile.neighbourhood)
+
+    return render(request, 'notifications.html', {"notifications":all_notifications})
+
+
+@login_required(login_url='/accounts/login')
+def create_profile(request):
+    current_user=request.user
+    if request.method == "POST":
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.username = current_user
+            profile.save()
+        return HttpResponseRedirect('/')
+
+    else:
+        form = ProfileForm()
+    return render(request, 'profile_form.html', {"form":form})
