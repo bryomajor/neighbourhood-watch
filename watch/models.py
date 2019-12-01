2ec9a9516e8a673fddadabb7ae915900309bedb8
Brian Otieno <bryomajor@gmail.com>
2019-12-01 11:39:43 +0300

    rectify the profile model

--- watch/models.py
+++ watch/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.dispatch import receiver
-from django.db.models.signals import post_save
+from tinymce.models import HTMLField
 
 # Create your models here.
 class Neighbourhood(models.Model):
@@ -9,18 +8,12 @@ class Neighbourhood(models.Model):
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    avatar = models.ImageField(upload_to='avatars/')
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    bio = HTMLField()
     neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
-    
-    @receiver(post_save, sender = User)
-    def create_user_profile(sender, instance, created, **kwargs):
-        if created:
-            Profile.objects.create(user=instance)
 
-    @receiver(post_save, sender = User)
-
-    def save_user_profile(sender, instance, **kwargs):
-        instance.profile.save()
-
-    def save_user_profile(self):
-        self.save()
+    def __str__(self):
+        return self.name
