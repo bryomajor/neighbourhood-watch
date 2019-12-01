51edf815af5cba01b90bc3619b0185fa880d83ac
Brian Otieno <bryomajor@gmail.com>
2019-12-01 12:23:11 +0300

    create methods that will save or delete a neighbourhood

--- watch/models.py
+++ watch/models.py
@@ -6,9 +6,19 @@ from tinymce.models import HTMLField
 class Neighbourhood(models.Model):
     neighbourhood_name = models.CharField(max_length = 60)
 
+    def __str__(self):
+        return self.neighbourhood_name
+
+    def create_neighbourhood(self):
+        self.save()
+        
+    @classmethod
+    def delete_neighbourhood(cls, neighbourhood_name):
+        cls.objects.filter(neighbourhood_name=neighbourhood_name).delete()
+
 
 class Profile(models.Model):
-    avatar = models.ImageField(upload_to='avatars/')
+    avatar = models.ImageField(upload_to='avatars/', blank = True)
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     email = models.EmailField()
